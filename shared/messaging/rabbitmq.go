9a6f99e70ea43559de554449c0ec070bff625ce7
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2026-01-30 08:52:31 +0000

    added implementation of messaging methods

---
+++ shared/messaging/rabbitmq.go
@@ -0,0 +1,112 @@
+package messaging
+
+import (
+	"context"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	conn    *amqp.Connection
+	Channel *amqp.Channel
+}
+
+func NewRabbitConnection(uri string) (*RabbitMQ, error) {
+	conn, err := amqp.Dial(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	rmq := &RabbitMQ{conn: conn,
+		Channel: ch}
+
+	if err := rmq.setupExchangesandQueues(); err != nil {
+		rmq.Close()
+		return nil, err
+	}
+
+	return rmq, nil
+}
+
+func (r *RabbitMQ) setupExchangesandQueues() error {
+	_, err := r.Channel.QueueDeclare(
+		"hello", // name
+		true,    // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RabbitMQ) PublishMessage(ctx context.Context, routingKey string, message string) error {
+	return r.Channel.PublishWithContext(ctx,
+		"",         // exchange
+		routingKey, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType:  "text/plain",
+			Body:         []byte(message),
+			DeliveryMode: amqp.Persistent,
+		})
+
+}
+
+type MessageHandler func(context.Context, amqp.Delivery) error
+
+func (r *RabbitMQ) ConsumeMessage(queueName string, handler MessageHandler) error {
+
+	msgs, err := r.Channel.Consume(
+		queueName, // queue
+		"",        // consumer
+		true,      // auto-ack
+		false,     // exclusive
+		false,     // no-local
+		false,     // no-wait
+		nil,       // args
+	)
+
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+
+	go func() {
+		for msg := range msgs {
+			log.Printf("Received a message: %s", msg.Body)
+			if err := handler(ctx, msg); err != nil {
+				log.Fatalf("failed to handle the msg : %v", err)
+			}
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+
+	return nil
+}
+
+func (r *RabbitMQ) Close() {
+	if r.conn != nil {
+		r.conn.Close()
+	}
+
+	if r.Channel != nil {
+		r.Channel.Close()
+	}
+
+}
